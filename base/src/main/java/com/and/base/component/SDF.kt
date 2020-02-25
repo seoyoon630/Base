9a41f29f3e60c7d17f457e320b4f730807c8cd42
정서윤 <seoyoon630@neobns.com>
2020-02-25 17:34:38 +0900

    component - SDF(SimpleDateFormat Util) initial Commit

--- base/src/main/java/com/and/base/component/SDF.kt
+++ base/src/main/java/com/and/base/component/SDF.kt
@@ -1,2 +1,36 @@
 package com.and.base.component
 
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+enum class SDF(pattern: String, locale: Locale = Locale.getDefault()) {
+    yyyymmdd("yyyyMMdd"),
+    yyyymmdd_("yyyy/MM/dd"),
+    yyyymmdd__("yyyy년 MM월 dd일"),
+    yyyymmdd_1("yyyy-MM-dd"),
+    yyyymmdd_2("yyyy.MM.dd"),
+
+    yyyymmddhhmm("yyyyMMddHHmm"),
+    yyyymmddhhmm_1("yyyy-MM-dd HH:mm"),
+    yyyymmddhhmm_2("yyyy.MM.dd HH:mm"),
+
+    yyyymmddhhmmss("yyyyMMddHHmmss"),
+    yyyymmddhhmmss_("yyyy/MM/dd HH:mm:ss"),
+    yyyymmddhhmmss_1("yyyy-MM-dd HH:mm:ss"),
+    yyyymmddhhmmss_2("yyyy.MM.dd HH:mm:ss"),
+    ddmmyyyyhhmmss_2("dd/MM/yy HH:mm:ss");
+
+    private val sdf: SimpleDateFormat = SimpleDateFormat(pattern, locale)
+
+    fun format(date: Date?): String = date?.let { sdf.format(date) } ?: ""
+
+    fun format(milliseconds: Long?): String = milliseconds?.let{format(Date(milliseconds))} ?: ""
+
+    @Throws(ParseException::class)
+    fun parseDate(date: String): Date = sdf.parse(date)
+
+    @Throws(ParseException::class)
+    fun parse(date: String): Long = sdf.parse(date).time
+}
+
