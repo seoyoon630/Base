b969066cf17279ca2c338288f4bdb599d1dfc863
박상혁 <psh161@naver.com>
2020-05-20 14:11:13 +0900

    - dp2px, sp2px Base Ext 추가

--- base/src/main/java/com/and/base/common/BaseExt.kt
+++ base/src/main/java/com/and/base/common/BaseExt.kt
@@ -1,6 +1,8 @@
 package com.and.base.common
 
 import android.content.Context
+import android.util.DisplayMetrics
+import android.util.TypedValue
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
@@ -13,4 +15,14 @@ fun View.showKeyboard() {
 fun View.hideKeyboard() {
     val imm: InputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(windowToken, 0)
+}
+
+fun Context.dp2px(dp: Float): Int {
+    val display_metrics: DisplayMetrics = resources.displayMetrics
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, display_metrics).toInt()
+}
+
+fun Context.sp2px(sp: Float): Int {
+    val display_metrics: DisplayMetrics = resources.displayMetrics
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, display_metrics).toInt()
 }
